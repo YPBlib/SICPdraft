088a4a53f136cdb40bf472486bc4b1ed36a7d7e8
YPBlib <ypb1024@foxmail.com>
2017-11-01 09:57:32 +0800

    m

--- SICP_draft.scm
+++ SICP_draft.scm
@@ -1,10 +1,52 @@
-formal parameter
-actual argument
+;formal parameter
+;actual argument
 
-normal order
-applicative order
+;applicative order: ﬁrst evaluates the operator and operands and then apply
+;normal order: fully expand and then reduce
 
 (define (p) (p) )
+;ConditionalExpressionsandPredicates
+(define (abs1 x)
+	(cond 
+		((> x 0) x)
+		((< x 0 )(- x))
+		((= x 0) 0) 
+	)
+)
+(define (abs2 x)
+	(cond 
+		((< x 0) (- x))
+		(else x)
+	)
+)
+
+(define (abs3 x)
+	(if (< x 0) (- x)
+		x
+	)
+)
+
+(define (predicatex1 x)
+	(and (< x 10) (> x 0) )
+)
+
+(define (predicatex2 x)
+	(or (> x 10) (< x 0) )
+)
+
+(define (predicatex3 x)
+	(not (= x 0))
+)
+
+(define (>= x y)
+	(or (= x y) (> x y) )
+)
+
+(define (<= x y)
+	(not (> x y) )
+)
+
+
 
 (define
 	(test x y )
